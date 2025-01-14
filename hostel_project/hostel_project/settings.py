b2dfd9d922780e73840e7f53e780eca5a1e0510e
FirasMoosaMohammed <firasmohamed0@gmail.com>
2025-01-14 10:52:13 +0530

    Updated

--- hostel_project/hostel_project/settings.py
+++ hostel_project/hostel_project/settings.py
@@ -124,13 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_USE_TLS=True
-EMAIL_PORT=587
-EMAIL_HOST_USER="firasmohamed0@gmail.com"
-EMAIL_HOST_PASSWORD="aryvxtfozahdekpw"
 
 
-RAZORPAY_KEY_ID = "rzp_test_UEcbN83WZm5fMR"
-RAZORPAY_KEY_SECRET = "sMB5DJoLiWYjypfJrkviKcjP"
+
+
